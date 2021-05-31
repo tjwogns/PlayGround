f39d609d621a2ea8acf122d73dd7b4a7ff01265e
서재훈 <tjwogns0820@mailplug.co.kr>
2021-05-31 17:27:21 +0900

    add compose folder

--- app/src/main/java/com/example/playground/MainActivity.kt
+++ app/src/main/java/com/example/playground/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.playground.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        println("It is First Commit")
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setListener()
+
+    }
+
+    private fun setListener() {
+        binding.tvCompose.setOnClickListener { view ->
+            Toast.makeText(this, "Compose", Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/example/playground/compose/ComposeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.playground.compose
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ComposeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+//        setContent {
+//
+//        }
+    }
+}
