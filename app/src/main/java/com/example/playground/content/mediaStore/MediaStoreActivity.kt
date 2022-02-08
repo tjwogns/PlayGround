4159b44ef98102c0f631bc9153010e0e9496a772
tjwogns <tjwogns0820@gmail.com>
2022-02-08 10:19:43 +0900

    add mediaStore test

--- app/src/main/java/com/example/playground/content/mediaStore/MediaStoreActivity.kt
+++ app/src/main/java/com/example/playground/content/mediaStore/MediaStoreActivity.kt
@@ -1,7 +1,8 @@
 package com.example.playground.content.mediaStore
 
-import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import com.example.playground.R
 import com.example.playground.base.BaseActivity
 import com.example.playground.databinding.ActivityMediaStoreBinding
@@ -22,8 +23,16 @@ class MediaStoreActivity : BaseActivity<ActivityMediaStoreBinding, MediaStoreVie
     }
 
     private fun setListener() {
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            binding.tvPath.setOnClickListener {
+                println("!!! MediaStore.Downloads.EXTERNAL_CONTENT_URI : [${MediaStore.Downloads.EXTERNAL_CONTENT_URI}] !!!")
+                println("!!! MediaStore.Audio.Media.EXTERNAL_CONTENT_URI : [${MediaStore.Audio.Media.EXTERNAL_CONTENT_URI}] !!!")
+                println("!!! MediaStore.Images.Media.EXTERNAL_CONTENT_URI : [${MediaStore.Images.Media.EXTERNAL_CONTENT_URI}] !!!")
+                println("!!! MediaStore.Video.Media.EXTERNAL_CONTENT_URI : [${MediaStore.Video.Media.EXTERNAL_CONTENT_URI}] !!!")
+            }
+        }
     }
+
     companion object {
     }
 }
