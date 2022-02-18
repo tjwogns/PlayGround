3bfee51d0fa7fa73712fd43b6b3e985aec35a5fc
tjwogns <tjwogns0820@gmail.com>
2022-02-18 15:50:27 +0900

    compose1 practice

--- app/src/main/java/com/example/playground/content/compose/Compose1Activity.kt
+++ app/src/main/java/com/example/playground/content/compose/Compose1Activity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -21,8 +23,12 @@ class Compose1Activity : ComponentActivity() {
         setContent {
             MyTheme {
                 Row(modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center) {
+                    horizontalArrangement = Arrangement.Start) {
                     TextTest()
+
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        HelloContent()
+                    }
                 }
             }
         }
@@ -36,9 +42,29 @@ fun TextTest() {
         Text(
             modifier = Modifier
                 .width(150.dp)
-                .size(20.dp),
+                .size(20.dp)
+                .padding(bottom = 16.dp),
             text = "Hello",
             textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun HelloContent() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        var name by remember { mutableStateOf("") }
+        if (name.isNotEmpty()) {
+            Text(
+                text = "Hello, $name",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.h5
+            )
+        }
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Name") }
         )
     }
 }
