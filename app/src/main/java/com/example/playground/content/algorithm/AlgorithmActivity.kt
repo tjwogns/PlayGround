369dbb30ffed9646af282c876bcab9edfda26926
tjwogns <tjwogns0820@gmail.com>
2022-04-03 15:19:25 +0900

    algorithm
    - https://programmers.co.kr/learn/courses/30/lessons/72410

--- app/src/main/java/com/example/playground/content/algorithm/AlgorithmActivity.kt
+++ app/src/main/java/com/example/playground/content/algorithm/AlgorithmActivity.kt
@@ -47,6 +47,21 @@ class AlgorithmActivity: BaseActivity<ActivityAlgorithmBinding, AlgorithmViewMod
             algorithm02(lottos = lottos1, win_nums = win_nums1)
 
         }
+
+        binding.tvAlgorithm03.setOnClickListener {
+            val numIdCase1 = "...!@BaT#*..y.abcdefghijklm"
+            val numIdCase2 = "z-+.^."
+            val numIdCase3 = "=.="
+            val numIdCase4 = "123_.def"
+            val numIdCase5 = "abcdefghijklmn.p"
+
+            algorithm03(numIdCase1)
+            algorithm03(numIdCase2)
+            algorithm03(numIdCase3)
+            algorithm03(numIdCase4)
+            algorithm03(numIdCase5)
+
+        }
     }
 
 
@@ -128,6 +143,33 @@ class AlgorithmActivity: BaseActivity<ActivityAlgorithmBinding, AlgorithmViewMod
         return answer
     }
     // ============================================================================================
+
+    /**
+     * https://programmers.co.kr/learn/courses/30/lessons/72410
+     * 시작 시간 : 22.04.03 13:30
+     * 소요 시간 : 1시간 30분 소요
+     */
+    private fun algorithm03(new_id: String): String {
+        val answer = new_id
+            .lowercase() // 1단계 소문자로 치환
+            .replace(Regex("[~!@#\$%^&*()=+\\[{\\]}:?,<>/]"),"") // 2단계  알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+            .replace(Regex("\\.{2,}"), ".") // 3단계 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+            .run {
+                var id = this
+                if (id.isNotEmpty() && id.first().toString() == (".")) id = id.substring(1)
+                if (id.isNotEmpty() && id.last().toString() == (".")) id = id.substring(0, this.lastIndex)
+                // 4단계 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+                if (id.isEmpty()) id = "a" // 5 단계 빈 문자열이라면, new_id에 "a"를 대입합니다.
+                if (id.length > 15) id = id.substring(0, 15)
+                if (id.last().toString() == (".")) id = id.substring(0, id.lastIndex)
+                // 6단계 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다. + 마지막 (.) 제거
+                while (id.length < 3) id = id.plus(id.last())
+                return@run id
+            }
+
+        println("!!! anwser : $answer !!!")
+        return answer
+    }
 
     // ============================================================================================
 
