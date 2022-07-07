dc6707926693a330f2729dd82f9a1c361e3024c3
tjwogns <tjwogns0820@gmail.com>
2022-07-07 18:26:17 +0900

    [*] add rx test code

--- app/src/main/java/com/example/playground/content/rx/RxActivity.kt
+++ app/src/main/java/com/example/playground/content/rx/RxActivity.kt
@@ -6,6 +6,7 @@ import com.example.playground.R
 import com.example.playground.base.BaseActivity
 import com.example.playground.databinding.ActivityRxBinding
 import io.reactivex.Observable
+import io.reactivex.functions.Consumer
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RxActivity : BaseActivity<ActivityRxBinding, RxViewModel>(
@@ -19,15 +20,32 @@ class RxActivity : BaseActivity<ActivityRxBinding, RxViewModel>(
         super.onCreate(savedInstanceState)
 
         setListener()
+
+        subscribeObservable()
     }
 
-    @SuppressLint("ClickableViewAccessibility")
+
+    @SuppressLint("ClickableViewAccessibility", "CheckResult")
     private fun setListener() {
         binding.tvEx1.setOnClickListener {
             Observable
                 .just("Hello?", "RxJava!?")
                 .map { it.dropLast(1) }
-                .subscribe { println(it) }
+                .subscribe (::println)
+        }
+
+        binding.tvObservableOnNext.setOnClickListener {
+            viewModel.observable.doOnNext{5}
+        }
+    }
+
+    /**
+     * 구독
+     */
+    @SuppressLint("CheckResult")
+    private fun subscribeObservable() {
+        viewModel.observable.subscribe {
+            println("$it")
         }
     }
 
--- app/src/main/java/com/example/playground/content/rx/RxViewModel.kt
+++ app/src/main/java/com/example/playground/content/rx/RxViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.playground.content.rx
 
 import com.tjwogns.presentation.base.BaseViewModel
+import io.reactivex.Observable
 
 class RxViewModel: BaseViewModel() {
 
+    val observable: Observable<Int> = Observable.create(){}
+//    {
+//        it.onNext(1)
+//        it.onNext(2)
+//        it.onNext(3)
+//    }
 }
