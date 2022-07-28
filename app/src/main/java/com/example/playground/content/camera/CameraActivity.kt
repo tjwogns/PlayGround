4069488dcffdeb4bcf8bc2055044f201438c639f
tjwogns <tjwogns0820@gmail.com>
2022-07-28 14:22:08 +0900

    [+] CameraX videoCapture test

--- app/src/main/java/com/example/playground/content/camera/CameraActivity.kt
+++ app/src/main/java/com/example/playground/content/camera/CameraActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playground.content.camera
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.pm.PackageManager
@@ -10,9 +11,10 @@ import android.util.Log
 import android.widget.Toast
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.video.Recorder
+import androidx.camera.video.*
 import androidx.camera.video.VideoCapture
 import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
 import com.example.playground.R
 import com.example.playground.base.BaseActivity
 import com.example.playground.databinding.ActivityCameraBinding
@@ -33,6 +35,7 @@ class CameraActivity : BaseActivity<ActivityCameraBinding, CameraViewModel>(
     private var imageCapture: ImageCapture? = null
 
     private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
 
     private lateinit var cameraExecutor: ExecutorService
 
@@ -105,7 +108,79 @@ class CameraActivity : BaseActivity<ActivityCameraBinding, CameraViewModel>(
         )
 
     }
-    private fun captureVideo() { }
+
+    private fun captureVideo() {
+        // 비디오 캡처에 대한 참조를 가져오고 만약 널이면 종료.
+        val videoCapture = this.videoCapture ?: return
+
+        // CameraX에서 요청 작업이 완료될 때까지 UI 비활성화. VideoRecordListener 내에서 다시 활성화
+        binding.tvVideoCapture.isEnabled = false
+
+        // 진행 중인 활성 recoding이 있으면 중지하고 해제.
+        val curRecording = recording
+        if (curRecording != null) {
+            // 현재 recording 세션 멈춤.
+            curRecording.stop()
+            recording = null
+            return
+        }
+
+        // 새로운 recording 세션 생성 및 시작
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.KOREA).format(System.currentTimeMillis())
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video")
+            }
+        }
+
+        // 저장할 곳을 지정. 다른앱에서 표시할 수 있도록 MediaStore에 저장
+        val mediaStoreOutputOptions = MediaStoreOutputOptions
+            .Builder(contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+            .setContentValues(contentValues)
+            .build()
+
+        recording = videoCapture.output
+            .prepareRecording(this, mediaStoreOutputOptions)
+            .apply {
+                if (PermissionChecker.checkSelfPermission(
+                        this@CameraActivity,
+                        Manifest.permission.RECORD_AUDIO
+                    ) == PermissionChecker.PERMISSION_GRANTED) {
+                    withAudioEnabled()
+                }
+            }.start(ContextCompat.getMainExecutor(this)) { recordEvent ->
+                // recoding 시작과 listener를 등록
+                when (recordEvent) {
+                    is VideoRecordEvent.Start -> {
+                        binding.tvVideoCapture.apply {
+                            text = getString(R.string.stop_capture)
+                            isEnabled = true
+                        }
+                    }
+                    is VideoRecordEvent.Finalize -> {
+                        if (!recordEvent.hasError()) {
+                            val msg = "Video capture 성공: ${recordEvent.outputResults.outputUri}"
+
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+
+                            Log.d(TAG, msg)
+                        } else {
+                            recording?.close()
+                            recording = null
+                            Log.e(TAG, "Video capture 실패, 에러: ${recordEvent.error}")
+                        }
+
+                        binding.tvVideoCapture.apply {
+                            text = getString(R.string.start_capture)
+                            isEnabled = true
+                        }
+                    }
+                }
+            }
+    }
 
     private fun startCamera() {
         // 카메라 수명주기를 바인딩하는데 사용. CameraX는 수명 주기를 인식하므로 카메라를 열고 닫는 작업이 필요 없음.
@@ -128,14 +203,19 @@ class CameraActivity : BaseActivity<ActivityCameraBinding, CameraViewModel>(
             imageCapture = ImageCapture.Builder()
                 .build()
 
-            // ImageAnalysis 객체 생성
-            val imageAnalyzer = ImageAnalysis.Builder()
+//            // ImageAnalysis 객체 생성
+//            val imageAnalyzer = ImageAnalysis.Builder()
+//                .build()
+//                .also {
+//                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+//                        Log.d(TAG, "Average luminosity: $luma")
+//                    })
+//                }
+
+            val recorder = Recorder.Builder()
+                .setQualitySelector(QualitySelector.from(Quality.HIGHEST))
                 .build()
-                .also {
-                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
-                        Log.d(TAG, "Average luminosity: $luma")
-                    })
-                }
+            videoCapture = VideoCapture.withOutput(recorder)
 
             // CameraSelector 객체 생성
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
@@ -147,8 +227,14 @@ class CameraActivity : BaseActivity<ActivityCameraBinding, CameraViewModel>(
                 // cameraSelector와 Preview 객체를 바인딩.
                 // imageCapture 바인딩
                 // imageAnalyzer 바인딩.
+//                cameraProvider.bindToLifecycle(
+//                    this, cameraSelector, preview, imageCapture, imageAnalyzer
+//                )
+
+                // cameraSelector와 Preview 객체를 바인딩.
+                // videoCapture 바인딩
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture, imageAnalyzer
+                    this, cameraSelector, preview, videoCapture
                 )
             } catch (exception: Exception) {
                 Log.e(TAG, "Use case binding failed", exception)
