795a088838b7f8e64c7ac1f69ae9999a6a2b7e93
tjwogns <tjwogns0820@gmail.com>
2022-09-22 17:33:02 +0900

    [*] DataStore Encrypt Test

--- app/src/main/java/com/example/playground/content/dataStore/DataStoreEncryptActivity.kt
+++ app/src/main/java/com/example/playground/content/dataStore/DataStoreEncryptActivity.kt
@@ -1,35 +1,90 @@
 package com.example.playground.content.dataStore
 
+import android.content.Context
 import android.os.Bundle
-import com.example.playground.R
-import com.example.playground.base.BaseActivity
-import com.example.playground.databinding.ActivityDataStoreEncryptBinding
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.datastore.dataStore
+import com.example.playground.content.compose.bottomNavigation.ui.theme.PlayGroundTheme
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
-class DataStoreEncryptActivity : BaseActivity<ActivityDataStoreEncryptBinding, DataStoreViewModel>(
-    R.layout.activity_data_store_encrypt
-) {
+class DataStoreEncryptActivity : ComponentActivity() {
+    private val Context.dataStore by dataStore(
+        fileName = "user-settings.json",
+        serializer = UserSettingsSerializer(CryptoManager())
+    )
 
-    override val viewModel: DataStoreViewModel by viewModel()
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
 
+        setContent {
+            PlayGroundTheme {
+                var userName by remember {
+                    mutableStateOf("")
+                }
+                var password by remember {
+                    mutableStateOf("")
+                }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setListener()
-        subscribeLiveData()
-    }
+                var settings by remember {
+                    mutableStateOf(UserSettings())
+                }
 
-    private fun setListener() {
+                val scope = rememberCoroutineScope()
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp)
+                ) {
+                    TextField(
+                        value = userName,
+                        onValueChange = { userName = it },
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = { Text(text = "Username") }
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    TextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = { Text(text = "Password") }
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row {
+                        Button(onClick = {
+                            scope.launch {
 
-    }
-
-    private fun subscribeLiveData() {
-        viewModel.booleanData.observe(this) { value ->
-
-        }
-
-        viewModel.protoInfoData.observe(this) {
-
+                                dataStore.updateData {
+                                    UserSettings(
+                                        username = userName,
+                                        password = password
+                                    )
+                                }
+                            }
+                        }) {
+                            Text(text = "Save")
+                        }
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Button(onClick = {
+                            scope.launch {
+                                settings = dataStore.data.first()
+                            }
+                        }) {
+                            Text(text = "Load")
+                        }
+                    }
+                    Text(text =  settings.toString())
+                }
+            }
         }
     }
 }
